b3ae88b56f9726ffe0e3ab4a1a1d131eef62af0a
chenleicpp <chenleicpp@163.com>
2015-07-20 17:27:36 +0800

    linshi

---
+++ app/src/main/java/com/sanshisoft/wenjiang/ui/NewsActivity.java
@@ -0,0 +1,22 @@
+package com.sanshisoft.wenjiang.ui;
+
+import android.os.Bundle;
+
+import com.sanshisoft.wenjiang.R;
+import com.sanshisoft.wenjiang.base.BaseActivity;
+
+/**
+ * Created by Administrator on 2015/7/20.
+ * 三级新闻列表页
+ */
+public class NewsActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_news;
+    }
+}
